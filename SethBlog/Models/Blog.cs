94c55c11975cf2b2fc685bf165feb65091c6923e
sbrevolution5 <sbrevolution5@aim.com>
2021-05-03 15:03:15 -0400

    added data annotations

--- SethBlog/Models/Blog.cs
+++ SethBlog/Models/Blog.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +9,21 @@ namespace SethBlog.Models
     public class Blog
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at most {1} characters. ", MinimumLength = 2)]
         public string Name { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at most {1} characters. ", MinimumLength = 2)]
         public string Description { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Created Date")]
         public DateTime Created { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Updated Date")]
         public DateTime? Updated { get; set; } //nullable because it will initially be null
     }
 }
--- SethBlog/Models/Post.cs
+++ SethBlog/Models/Post.cs
@@ -11,15 +11,28 @@ namespace SethBlog.Models
     {
         public int Id { get; set; }
         public int BlogId { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at most {1} characters. ", MinimumLength = 2)]
         public string Title { get; set; }
+
+        [Required]
+        [StringLength(500, ErrorMessage = "The {0} must be at least {2} and at most {1} characters. ", MinimumLength = 2)]
         public string Abstract { get; set; }
+        [Required]
         public string Content { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Created Date")]
         public DateTime Created { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Updated Date")]
         public DateTime? Updated { get; set; }
         public string Slug { get; set; } //Derived from title (will be programatically implemented) 
         //give myself ability to record state of any post
         //i.e. ready, published, draft
-        [Display(Name ="Publish State")]
+        [Display(Name ="Post State")]
         public PostState PostState { get; set; }
 
     }
