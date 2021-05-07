36ee82bc6542590f5347983170e95eb38afa88e0
sbrevolution5 <sbrevolution5@aim.com>
2021-05-07 16:02:45 -0400

    began dataservice to seed roles

---
+++ SethBlog/Enums/BlogRole.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SethBlog.Enums
+{
+    public enum BlogRole
+    {
+        Administrator,
+        Moderator
+    }
+}
---
+++ SethBlog/Services/DataService.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Identity;
+using SethBlog.Data;
+using SethBlog.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SethBlog.Services
+{
+    public class DataService
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public DataService(ApplicationDbContext context, RoleManager<IdentityRole> roleManager)
+        {
+            _context = context;
+            _roleManager = roleManager;
+        }
+
+        public async Task ManageDataAsync()
+        {
+            //Task 1: Seed roles (create and enter into Authorization system
+            await SeedRolesAsync();
+            // Task 2 seed a few users into AspNetUsers
+            await SeedUsersAsync();
+        }
+        private async Task SeedRolesAsync()
+        {
+            //Are there roles in the system? 
+            if (_context.Roles.Any())
+            {
+                return;
+            }
+            //Spin through enum and do stuff
+            foreach (var role in Enum.GetNames(typeof(BlogRole)))
+            {
+                //create Role in system for each role
+                await _roleManager.CreateAsync(new IdentityRole(role));
+            }
+        }
+        private async Task SeedUsersAsync()
+        {
+
+        }
+    }
+}
