77d688cf267a1bc1cf187970b7586a982bb87297
sbrevolution5 <sbrevolution5@aim.com>
2021-05-17 12:45:19 -0400

    updated migration

---
+++ SethBlog/Data/Migrations/20210517164456_013datatypedate.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SethBlog.Data.Migrations
+{
+    public partial class _013datatypedate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
