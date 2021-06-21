d93768f5b4bfe31fc40d6ac8e52317209c882a03
sbrevolution5 <sbrevolution5@aim.com>
2021-06-21 12:58:24 -0400

    Added api

---
+++ SethBlog/Controllers/API/PostsAPIController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SethBlog.Data;
+using SethBlog.Models;
+
+namespace SethBlog.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostsAPIController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PostsAPIController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/PostsAPI
+        [HttpGet("/GetTopXPosts/{num}")]
+        public async Task<ActionResult<IEnumerable<Post>>> GetTopXPosts(int num)
+        {
+            return await _context.Post.OrderByDescending(p=>p.PublishedDate).Take(num).ToListAsync();
+        }
+
+        // GET: api/PostsAPI/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Post>> GetPost(int id)
+        {
+            var post = await _context.Post.FindAsync(id);
+
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            return post;
+        }
+
+        //// PUT: api/PostsAPI/5
+        //// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        //[HttpPut("{id}")]
+        //public async Task<IActionResult> PutPost(int id, Post post)
+        //{
+        //    if (id != post.Id)
+        //    {
+        //        return BadRequest();
+        //    }
+
+        //    _context.Entry(post).State = EntityState.Modified;
+
+        //    try
+        //    {
+        //        await _context.SaveChangesAsync();
+        //    }
+        //    catch (DbUpdateConcurrencyException)
+        //    {
+        //        if (!PostExists(id))
+        //        {
+        //            return NotFound();
+        //        }
+        //        else
+        //        {
+        //            throw;
+        //        }
+        //    }
+
+        //    return NoContent();
+        //}
+
+        //// POST: api/PostsAPI
+        //// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        //[HttpPost]
+        //public async Task<ActionResult<Post>> PostPost(Post post)
+        //{
+        //    _context.Post.Add(post);
+        //    await _context.SaveChangesAsync();
+
+        //    return CreatedAtAction("GetPost", new { id = post.Id }, post);
+        //}
+
+        // DELETE: api/PostsAPI/5
+        //[HttpDelete("{id}")]
+        //public async Task<IActionResult> DeletePost(int id)
+        //{
+        //    var post = await _context.Post.FindAsync(id);
+        //    if (post == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    _context.Post.Remove(post);
+        //    await _context.SaveChangesAsync();
+
+        //    return NoContent();
+        //}
+
+        private bool PostExists(int id)
+        {
+            return _context.Post.Any(e => e.Id == id);
+        }
+    }
+}
--- SethBlog/Startup.cs
+++ SethBlog/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using SethBlog.Data;
 using SethBlog.Models;
 using SethBlog.Services;
@@ -49,7 +50,18 @@ namespace SethBlog
             services.AddScoped<IEmailSender, GmailEmailService>();
             services.AddScoped<SearchService>();
             services.AddScoped<ReadTimeService>();
-
+            services.AddSwaggerGen(c =>
+            c.SwaggerDoc("v1", new OpenApiInfo {
+                Title = "Blog API",
+                Version = "v1",
+                Description = "Infusing users with data via The Coder's Codex API",
+                Contact = new OpenApiContact {
+                    Email = "sethbcoding@gmail.com",
+                    Name = "Seth Burleson",
+                    Url = new System.Uri("https://www.linkedin.com/in/seth-a-burleson-a1555211a/")
+                    }
+            }));
+            
 
         }
 
@@ -67,6 +79,12 @@ namespace SethBlog
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "TestAPI v1");
+                c.DocumentTitle = "The Coder's Codex Public API";
+                });
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
